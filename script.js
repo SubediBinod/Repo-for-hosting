88c468ac2d61a74f519d6a947a2717c0917c7d66
Binod <bidhaksubedi77@gmail.com>
2024-04-03 22:40:57 +0530

    updated hamburger css

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
+const hamburger=document.querySelector(".hamburger");
+const navMenu=document.querySelector(".nav-menu");
+hamburger.addEventListener("click",()=>{
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+
+})
+
 var typed = new Typed(".typing-2", {
   strings: [" 👨‍💻 A Tech Enthusiast ","👨‍🎓 A Computer Engineering student ", " 🚀 Aspiring Web-Developer","🎨 UI Designer"],
   typeSpeed: 100,
   backSpeed: 60,
   loop: true
 });
+document.querySelectorAll(".nav-link").forEach(n=>n.addEventListener("click",()=>{
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+
+}))
